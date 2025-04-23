bdcb724b89db421e197cbf2c48426a02d7637542
adlibalibi <charithra.sm@gmail.com>
2025-04-23 22:34:43 +0530

    Add Firebase Auth

---
+++ frontend/src/firebase/firebase.js
@@ -0,0 +1,18 @@
+import { initializeApp } from "firebase/app";
+import { getAuth, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBSIuddlcYzJH14b7qc2Pzz8EN7Tizj0t4",
+  authDomain: "neopos--auth.firebaseapp.com",
+  projectId: "neopos--auth",
+  storageBucket: "neopos--auth.firebasestorage.app",
+  messagingSenderId: "229092865466",
+  appId: "1:229092865466:web:7e09a0d004002b1c7fd983",
+  measurementId: "G-4ZT6MGR7KZ"
+};
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const provider = new GoogleAuthProvider();
+
+export { auth, provider };
