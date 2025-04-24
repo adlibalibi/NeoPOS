23b1b698361abfa0b1c5a0d93697c3d0d79b0210
Charithra <135539349+adlibalibi@users.noreply.github.com>
2025-04-25 01:18:29 +0530

    Update app.py -metrics endpoint

--- backend/app.py
+++ backend/app.py
@@ -1,30 +1,25 @@
-from flask import Flask
-#from inventory import inventory_bp
-#from billing import billing_bp
-#from users import users_bp
+from flask import Flask, Response, request
 from payment import payment_bp
 from flask_cors import CORS
-#import firebase_admin
 import os
-#from firebase_admin import credentials, firestore
 
-#cred_path = os.path.join(os.path.dirname(__file__), 'fb_key.json')
-
-#if not firebase_admin._apps:
- #   cred = credentials.Certificate(cred_path)
-  #  firebase_admin.initialize_app(cred)
-
-#db = firestore.client()
+from prometheus_client import Counter, generate_latest, CONTENT_TYPE_LATEST
 
 app = Flask(__name__)
 CORS(app, supports_credentials=True)
 
-#app.register_blueprint(inventory_bp, url_prefix="/inventory")
-#app.register_blueprint(billing_bp, url_prefix="/billing")
 app.register_blueprint(payment_bp, url_prefix="/payment")
-#app.register_blueprint(users_bp, url_prefix="/users")
+
+REQUEST_COUNT = Counter('http_requests_total', 'Total HTTP requests', ['method', 'endpoint'])
+
+@app.before_request
+def before_request_func():
+    REQUEST_COUNT.labels(method=request.method, endpoint=request.path).inc()
+
+@app.route("/metrics")
+def metrics():
+    return Response(generate_latest(), mimetype=CONTENT_TYPE_LATEST)
 
 if __name__ == "__main__":
-    # Get the PORT from environment variable (Render will assign this)
-    port = int(os.environ.get("PORT", 5050))  # Default to 5050 if not set
+    port = int(os.environ.get("PORT", 5050))
     app.run(host="0.0.0.0", port=port, debug=True)
